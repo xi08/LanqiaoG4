0cf69ee2c8cbc74761d22000df852803568281fe
xi08 <dinghf@sues.edu.cn>
2023-03-15 18:44:53 +0800

    add boot time counter

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -20,9 +20,9 @@
 #include "main.h"
 #include "adc.h"
 #include "dma.h"
+#include "gpio.h"
 #include "tim.h"
 #include "usart.h"
-#include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
@@ -50,7 +50,10 @@
 
 /* USER CODE BEGIN PV */
 volatile uint8_t sysTime_msFlag;
-volatile uint32_t sysTime;
+volatile uint32_t sysTime = 0;
+
+uint8_t bootTime[4] = {0};
+const uint8_t bootTimePos[4] = {'S', 'u', 'e', 's'};
 
 extern keyState_enum keyState[keyNum];
 uint8_t ledBuffer = 0x1;
@@ -59,6 +62,7 @@ uint8_t dispBuffer[21];
 uint16_t adc1Val[2], adc2Val, r39Val, r40Val;
 float r37V, r38V, mcpV, mcpR;
 uint8_t mcp_res;
+
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -75,67 +79,96 @@ void ledUpdate(uint8_t led);
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
-  /* USER CODE BEGIN 1 */
+    /* USER CODE BEGIN 1 */
+    uint8_t main_i = 0;
+    /* USER CODE END 1 */
 
-  /* USER CODE END 1 */
+    /* MCU Configuration--------------------------------------------------------*/
 
-  /* MCU Configuration--------------------------------------------------------*/
+    /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+    HAL_Init();
 
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  HAL_Init();
+    /* USER CODE BEGIN Init */
 
-  /* USER CODE BEGIN Init */
+    /* USER CODE END Init */
 
-  /* USER CODE END Init */
+    /* Configure the system clock */
+    SystemClock_Config();
 
-  /* Configure the system clock */
-  SystemClock_Config();
+    /* USER CODE BEGIN SysInit */
 
-  /* USER CODE BEGIN SysInit */
+    /* USER CODE END SysInit */
 
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  MX_GPIO_Init();
-  MX_DMA_Init();
-  MX_TIM2_Init();
-  MX_USART1_UART_Init();
-  MX_ADC1_Init();
-  MX_ADC2_Init();
-  MX_TIM3_Init();
-  /* USER CODE BEGIN 2 */
+    /* Initialize all configured peripherals */
+    MX_GPIO_Init();
+    MX_DMA_Init();
+    MX_TIM2_Init();
+    MX_USART1_UART_Init();
+    MX_ADC1_Init();
+    MX_ADC2_Init();
+    MX_TIM3_Init();
+    /* USER CODE BEGIN 2 */
     LCD_Init();
-    I2CInit();
     LCD_Clear(White);
+
+    I2CInit();
+    for (main_i = 0; main_i < 4; main_i++)
+    {
+        bootTime[main_i] = eeRead(bootTimePos[main_i]);
+        HAL_Delay(1);
+        if (bootTime[main_i] != bootTime[0])
+        {
+            bootTime[0] = 0;
+            break;
+        }
+    }
+    bootTime[0]++;
+    for (main_i = 0; main_i < 4; main_i++)
+    {
+        eeWrite(bootTimePos[main_i], bootTime[0]);
+        HAL_Delay(1);
+    }
+
+    ledBuffer = eeRead(0x00);
     ledUpdate(ledBuffer);
 
-    LCD_DisplayStringLine(Line0, (uint8_t *)"CT117E-M4");
+    mcp_res = mcpRead();
+    mcpR = mcp_res * 0.7874;
+
+    sprintf((char *)dispBuffer, "%s  Boot=%u", "CT117E-M4", bootTime[0]);
+    LCD_DisplayStringLine(Line0, dispBuffer);
+    printf("%s\n", dispBuffer);
     LCD_DisplayStringLine(Line1, (uint8_t *)"Compiled in");
     sprintf((char *)dispBuffer, "%s,%s", __TIME__, __DATE__);
     LCD_DisplayStringLine(Line2, dispBuffer);
-    printf("%s\n%s\n%s\n", "CT117E-M4", "Compiled in", dispBuffer);
+    printf("%s\n%s\n", "Compiled in", dispBuffer);
     HAL_Delay(1000);
     LCD_ClearLine(Line1);
     LCD_ClearLine(Line2);
 
     HAL_ADC_Start_DMA(&hadc1, (uint32_t *)adc1Val, 2);
     HAL_ADC_Start_DMA(&hadc2, (uint32_t *)&adc2Val, 1);
+
     HAL_TIM_IC_Start_IT(&htim2, TIM_CHANNEL_1);
     HAL_TIM_IC_Start_IT(&htim3, TIM_CHANNEL_1);
 
     printf("Init OK\n");
 
-  /* USER CODE END 2 */
+    /* USER CODE END 2 */
 
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
+    /* Infinite loop */
+    /* USER CODE BEGIN WHILE */
     while (1)
     {
+        r37V = adc2Val * 3.3 / 4095;
+        r38V = adc1Val[0] * 3.3 / 4095;
+        mcpV = adc1Val[1] * 3.3 / 4095;
+
         if (sysTime_msFlag)
         {
             sysTime_msFlag = 0;
@@ -167,64 +200,63 @@ int main(void)
             printf("R39=%uHz,R40=%uHz\n", r39Val, r40Val);
             printf("MCP=%.2fV,%.2fK\n", mcpV, mcpR);
         }
-    /* USER CODE END WHILE */
+        /* USER CODE END WHILE */
 
-    /* USER CODE BEGIN 3 */
+        /* USER CODE BEGIN 3 */
         keyUpdate();
         keyResp();
+
         ledUpdate(ledBuffer);
+        eeWrite(0x00, ledBuffer);
+
         mcp_res = mcpRead();
         mcpR = mcp_res * (7.874e-1);
-        r37V = adc2Val * 3.3 / 4095;
-        r38V = adc1Val[0] * 3.3 / 4095;
-        mcpV = adc1Val[1] * 3.3 / 4095;
     }
-  /* USER CODE END 3 */
+    /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+    RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+    RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
+    /** Configure the main internal regulator output voltage
+     */
+    HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1);
 
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
-  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
-  RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV3;
-  RCC_OscInitStruct.PLL.PLLN = 20;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
-  RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    /** Initializes the RCC Oscillators according to the specified parameters
+     * in the RCC_OscInitTypeDef structure.
+     */
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM = RCC_PLLM_DIV3;
+    RCC_OscInitStruct.PLL.PLLN = 20;
+    RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ = RCC_PLLQ_DIV2;
+    RCC_OscInitStruct.PLL.PLLR = RCC_PLLR_DIV2;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
 
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    /** Initializes the CPU, AHB and APB buses clocks
+     */
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
-  {
-    Error_Handler();
-  }
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+    {
+        Error_Handler();
+    }
 }
 
 /* USER CODE BEGIN 4 */
@@ -237,6 +269,7 @@ void keyResp(void)
 
         LCD_ClearLine(Line7);
         LCD_DisplayStringLine(Line7, (uint8_t *)"B1 Short");
+        // act.
         ledBuffer <<= 1;
         if (ledBuffer == 0x0)
             ledBuffer = 0x01;
@@ -247,6 +280,9 @@ void keyResp(void)
     case S4: // Long
         LCD_ClearLine(Line7);
         LCD_DisplayStringLine(Line7, (uint8_t *)"B1 Long");
+        // act.
+        ledBuffer = 0x00;
+
         keyState[0] = S0; // reset state
         break;
 
@@ -260,6 +296,7 @@ void keyResp(void)
     case S3: // Short
         LCD_ClearLine(Line7);
         LCD_DisplayStringLine(Line7, (uint8_t *)"B2 Short");
+        // act.
         ledBuffer >>= 1;
         if (ledBuffer == 0x0)
             ledBuffer = 0x80;
@@ -270,7 +307,12 @@ void keyResp(void)
     case S4: // Long
         LCD_ClearLine(Line7);
         LCD_DisplayStringLine(Line7, (uint8_t *)"B2 Long");
+
+        // act.
+        ledBuffer = 0xff;
+
         keyState[1] = S0; // reset state
+
         break;
 
     default:
@@ -283,13 +325,19 @@ void keyResp(void)
     case S3: // Short
         LCD_ClearLine(Line7);
         LCD_DisplayStringLine(Line7, (uint8_t *)"B3 Short");
+        // act.
         mcpWrite(--mcp_res);
+
         keyState[2] = S0; // reset state
         break;
 
     case S4: // Long
         LCD_ClearLine(Line7);
         LCD_DisplayStringLine(Line7, (uint8_t *)"B3 Long");
+        // act.
+        mcp_res = 0;
+        mcpWrite(mcp_res);
+
         keyState[2] = S0; // reset state
         break;
 
@@ -303,13 +351,19 @@ void keyResp(void)
     case S3: // Short
         LCD_ClearLine(Line7);
         LCD_DisplayStringLine(Line7, (uint8_t *)"B4 Short");
+        // act.
         mcpWrite(++mcp_res);
+
         keyState[3] = S0; // reset state
         break;
 
     case S4: // Long
         LCD_ClearLine(Line7);
         LCD_DisplayStringLine(Line7, (uint8_t *)"B4 Long");
+        // act.
+        mcp_res = 0x7f;
+        mcpWrite(mcp_res);
+
         keyState[3] = S0; // reset state
         break;
 
@@ -339,39 +393,39 @@ void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
 
 int fputc(int ch, FILE *f)
 {
-    HAL_UART_Transmit(&huart1,(uint8_t*)&ch,1,0xffff);
+    HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xffff);
     return ch;
 }
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
-  /* USER CODE BEGIN Error_Handler_Debug */
+    /* USER CODE BEGIN Error_Handler_Debug */
     /* User can add his own implementation to report the HAL error return state */
     __disable_irq();
     while (1)
     {
     }
-  /* USER CODE END Error_Handler_Debug */
+    /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
-  /* USER CODE BEGIN 6 */
+    /* USER CODE BEGIN 6 */
     /* User can add his own implementation to report the file name and line number,
        ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-  /* USER CODE END 6 */
+    /* USER CODE END 6 */
 }
 #endif /* USE_FULL_ASSERT */
--- Core/Src/stm32g4xx_it.c
+++ Core/Src/stm32g4xx_it.c
@@ -55,8 +55,6 @@
 /* USER CODE END 0 */
 
 /* External variables --------------------------------------------------------*/
-extern DMA_HandleTypeDef hdma_adc1;
-extern DMA_HandleTypeDef hdma_adc2;
 extern TIM_HandleTypeDef htim2;
 extern TIM_HandleTypeDef htim3;
 /* USER CODE BEGIN EV */
@@ -202,34 +200,6 @@ void SysTick_Handler(void)
 /* For the available peripheral interrupt handler names,                      */
 /* please refer to the startup file (startup_stm32g4xx.s).                    */
 /******************************************************************************/
-
-/**
-  * @brief This function handles DMA1 channel1 global interrupt.
-  */
-void DMA1_Channel1_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA1_Channel1_IRQn 0 */
-
-  /* USER CODE END DMA1_Channel1_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_adc1);
-  /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
-
-  /* USER CODE END DMA1_Channel1_IRQn 1 */
-}
-
-/**
-  * @brief This function handles DMA1 channel2 global interrupt.
-  */
-void DMA1_Channel2_IRQHandler(void)
-{
-  /* USER CODE BEGIN DMA1_Channel2_IRQn 0 */
-
-  /* USER CODE END DMA1_Channel2_IRQn 0 */
-  HAL_DMA_IRQHandler(&hdma_adc2);
-  /* USER CODE BEGIN DMA1_Channel2_IRQn 1 */
-
-  /* USER CODE END DMA1_Channel2_IRQn 1 */
-}
 
 /**
   * @brief This function handles TIM2 global interrupt.
